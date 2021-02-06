82ee156473397ff5d6ee413ea552cdd00f0a71a1
László Görög <contact@nergal.xyz>
2021-02-06 19:08:15 +0100

    ebml: enhance decode test

--- matroska/schema_test.go
+++ matroska/schema_test.go
@@ -2,21 +2,76 @@ package matroska
 
 import (
 	"github.com/coding-socks/ebml"
-	"log"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"testing"
 )
 
-func TestDecode(t *testing.T) {
-	f, err := os.Open(filepath.Join("..", "test5.mkv"))
+func downloadTestFile(filename, source string) error {
+	resp, err := http.Get(source)
 	if err != nil {
-		t.Fatal(err)
+		return err
+	}
+	defer resp.Body.Close()
+	f, err := os.Create(filepath.Join(".", filename))
+	if err != nil {
+		return err
 	}
 	defer f.Close()
-	var d Document
-	if err := ebml.NewDecoder(f).Decode(&d); err != nil {
-		t.Fatal(err)
+	_, err = io.Copy(f, resp.Body)
+	return err
+}
+
+func TestDecode(t *testing.T) {
+	var err error
+	var f *os.File
+	testFiles := []struct {
+		name     string
+		filename string
+		source   string
+	}{
+		{
+			name:     "Live stream recording",
+			filename: "test4.mkv",
+			source:   "https://github.com/Matroska-Org/matroska-test-files/blob/master/test_files/test4.mkv?raw=true",
+		},
+		{
+			name:     "Multiple audio/subtitles",
+			filename: "test5.mkv",
+			source:   "https://github.com/Matroska-Org/matroska-test-files/blob/master/test_files/test5.mkv?raw=true",
+		},
+		{
+			name:     "Different EBML head sizes & cue-less seeking",
+			filename: "test6.mkv",
+			source:   "https://github.com/Matroska-Org/matroska-test-files/blob/master/test_files/test6.mkv?raw=true",
+		},
 	}
-	log.Printf("%+v", d.EBML)
+	for _, tt := range testFiles {
+		p := filepath.Join(".", tt.filename)
+		_, err := os.Stat(p)
+		if err == nil {
+			continue
+		}
+		if !os.IsNotExist(err) {
+			t.Fatal(err)
+		}
+		if err := downloadTestFile(tt.filename, tt.source); err != nil {
+			t.Fatal(err)
+		}
+	}
+	for _, tt := range testFiles {
+		t.Run(tt.name, func(t *testing.T) {
+			f, err = os.Open(filepath.Join(".", tt.filename))
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
+			var d Document
+			if err = ebml.NewDecoder(f).Decode(&d); err != nil {
+				t.Error(err)
+			}
+		})
+	}
 }
