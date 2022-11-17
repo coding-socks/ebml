c5a751184135b59c3958e8b68fce1d3a8907e749
László Görög <contact@nergal.xyz>
2022-11-17 21:59:20 +0100

    ebml: fix test

--- ebml_test.go
+++ ebml_test.go
@@ -55,7 +55,7 @@ func TestReadElementID(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := ReadElementID(tt.args.r, tt.args.maxIDLength)
+			got, _, err := ReadElementID(tt.args.r, tt.args.maxIDLength)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ReadElementID() error = %v, wantErr %v", err, tt.wantErr)
 				return
